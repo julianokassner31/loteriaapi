d140d1a8d064d943a0e933065d321f6683cefcb4
jkassner <julianokassner@hotmail.com>
2020-03-01 17:52:59 -0300

    Configuration datasource heroku

--- src/main/java/jkassner/com/br/apiloteria/config/ConfigDataSourceHeroku.java
+++ src/main/java/jkassner/com/br/apiloteria/config/ConfigDataSourceHeroku.java
@@ -1,28 +1,33 @@
 package jkassner.com.br.apiloteria.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import javax.sql.DataSource;
 
 @Configuration
 @Profile("heroku")
 public class ConfigDataSourceHeroku {
 
     @Bean
-    public DataSource getDataSource() {
+    public DataSource getDataSource() throws URISyntaxException {
 
-        String url = System.getenv("DATABASE_URL");
-        String user = System.getenv("USER_DB");
-        String pass = System.getenv("PASS_DB");
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url(url);
-        dataSourceBuilder.username(user);
-        dataSourceBuilder.password(pass);
+        dataSourceBuilder.url(dbUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
